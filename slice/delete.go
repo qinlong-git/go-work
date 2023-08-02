b10301d52886167b3981a4277cb8b90b363c7e48
LongGeGe01 <9658941+longgege01@user.noreply.gitee.com>
2023-08-02 14:47:39 +0800

    改造成泛型方法

--- slice/delete.go
+++ slice/delete.go
@@ -10,3 +10,12 @@ func delete(slice []string, index int) ([]string, error) {
 	}
 	return append(slice[:index], slice[index+1:]...), nil
 }
+
+// deleteV1 改造成泛型方法
+func deleteV1[T any](slice []T, index int) ([]T, error) {
+	//判断下标是否越界
+	if index < 0 || index >= len(slice) {
+		return nil, errs.NewErrIndexOutOfRange(len(slice), index)
+	}
+	return append(slice[:index], slice[index+1:]...), nil
+}
--- slice/delete_test.go
+++ slice/delete_test.go
@@ -14,3 +14,17 @@ func TestDelete(t *testing.T) {
 	//output:[1 2 4 5]
 	//[1 2 3 4 5]"}
 }
+
+func TestDeleteV1(t *testing.T) {
+	s := []int32{1, 2, 3, 4, 5}
+	sli, err := deleteV1(s, 2)
+	t.Log(sli)
+	t.Logf("%v", sli)
+	fmt.Printf("%v,错误是：%v", sli, err)
+	s1 := []string{"1", "2", "3", "4", "5"}
+	sli1, err1 := deleteV1(s1, 2)
+	t.Log(sli1)
+	t.Logf("%v", sli1)
+	fmt.Printf("%v,错误是：%v", sli1, err1)
+
+}
